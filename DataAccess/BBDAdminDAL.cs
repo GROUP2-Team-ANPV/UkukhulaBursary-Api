c375ebfef2d2ceb33e3d0e83d08619e77fa7d1b5
Ndzalama Mabasa <ndzalamabasa@gmail.com>
2024-03-04 19:50:22 +0200

    restore working version for allocation

--- DataAccess/BBDAdminDAL.cs
+++ DataAccess/BBDAdminDAL.cs
@@ -43,7 +43,6 @@ namespace DataAccess
                 }
                 _connection.Close();
                 return requests;
-
             }
             finally
             {
@@ -77,7 +76,6 @@ namespace DataAccess
                 }
             }
             catch (Exception ex)
-
             {
                 Console.WriteLine($"Error executing AddUniversityAndUser: {ex.Message}");
             }
@@ -283,6 +281,92 @@ namespace DataAccess
                 _connection.Close();
             }
         }
+
+
+
+        // public void AllocateFunds()
+        // {
+        //     try
+        //     {
+        //         _connection.Open();
+
+
+        //         string countQuery = "SELECT COUNT(*) FROM dbo.University";
+        //         int totalUniversities;
+        //         using (SqlCommand countCommand = new SqlCommand(countQuery, _connection))
+        //         {
+        //             totalUniversities = (int)countCommand.ExecuteScalar();
+        //         }
+
+
+        //         string budgetQuery = "SELECT Budget FROM dbo.BBDAllocation WHERE Year= 2024";
+        //         decimal budget;
+        //         using (SqlCommand budgetCommand = new SqlCommand(budgetQuery, _connection))
+        //         {
+        //             budget = (decimal)budgetCommand.ExecuteScalar();
+        //         }
+
+
+        //         decimal equalAmount = budget / totalUniversities;
+
+
+        //         string insertOrUpdateQuery = @"
+        //         UPDATE UFA
+        //         SET Budget = @EqualAmount
+        //         FROM dbo.UniversityFundAllocation UFA
+        //         INNER JOIN dbo.University U ON UFA.UniversityID = U.ID
+        //         WHERE UFA.UniversityID = @UniversityID;";
+
+        //         using (SqlCommand command = new SqlCommand(insertOrUpdateQuery, _connection))
+        //         {
+        //             command.Parameters.AddWithValue("@EqualAmount", equalAmount);
+
+
+        //                     SqlParameter universityIdParameter = command.Parameters.AddWithValue("@UniversityID", SqlDbType.Int);
+
+        //                     for (int universityID = 1; universityID <= totalUniversities; universityID++)
+        //                     {
+
+        //                         universityIdParameter.Value = universityID;
+        //                         command.ExecuteNonQuery();
+        //                     }
+
+
+        //         }
+        //     }
+        //     catch (Exception ex)
+        //     {
+        //         Console.WriteLine($"Error allocating funds: {ex.Message}");
+        //     }
+        //     finally
+        //     {
+        //         _connection.Close();
+        //     }
+        // }
+
+        public void AllocateFunds()
+        {
+
+            try
+            {
+                _connection.Open();
+
+
+                using (SqlCommand command = new SqlCommand("DistributeFundsEqually", _connection))
+                {
+                    command.CommandType = CommandType.StoredProcedure;
+
+                    _connection.Open();
+                    command.ExecuteNonQuery();
+                }
+            }
+
+            catch (Exception ex)
+            {
+                Console.WriteLine("An error occurred: " + ex.Message);
+            }
+        }
+
 
 
         public void AllocateUniversityFunds(AllocateFunds dataAccessModel)
